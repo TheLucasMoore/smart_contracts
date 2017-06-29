7e8b70b1889e5cf1a0e17911d46e33dfa68828f3
TheLucasMoore <lucasedwardmoore@gmail.com>
2017-06-28 21:42:45 -0600

    add auth scheme

--- Escrow.sol
+++ Escrow.sol
@@ -10,4 +10,20 @@ contract Escrow {
     seller = _seller;
     arbiter = _arbiter;
   }
+
+  function payoutToSeller() {
+    if(msg.sender == buyer || msg.sender == arbiter) {
+      seller.send(this.balance);
+    }
+  }
+
+  function refundToBuyer() {
+    if(msg.sender == seller || msg.sender == arbiter) {
+      buyer.send(this.balance);
+    }
+  }
+
+  function getBalance() constant returns (uint) {
+    return this.balance;
+  }
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+# Ethereum Smart Contacts
+
+Check out [decypher.tv](http://decypher.tv/series/ethereum-development)
+
+For the dev environment:
+```javascript
+> testrpc
+> node environment.js
+```
+
+For the Dapp:
+
+```python
+cd simple_dapp
+python -m SimpleHTTPServer 3000
+```
